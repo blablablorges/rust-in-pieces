a3b0505c270e69ed62f9c5813c3956172cf2805b
Sebastian Werner <werner@tu-berlin.de>
2026-01-19 14:28:04 +0100

    some cleanup

--- src/shippingserwasm/lib/wasi-grpc-server/src/lib.rs
+++ src/shippingserwasm/lib/wasi-grpc-server/src/lib.rs
@@ -44,6 +44,11 @@ pub fn grpc_component(attr: TokenStream, item: TokenStream) -> TokenStream {
 
             impl ::wasi::exports::http::incoming_handler::Guest for #wasi_implementor {
                 fn handle(request: ::wasi::exports::http::incoming_handler::IncomingRequest, response_out: ::wasi::exports::http::incoming_handler::ResponseOutparam) {
+                    static INIT: ::std::sync::Once = ::std::sync::Once::new();
+                    INIT.call_once(|| {
+                        println!(concat!(stringify!(#http_impl_struct_name), " gRPC component initialized and ready to handle requests"));
+                    });
+                    println!(concat!(stringify!(#http_impl_struct_name), " handling incoming request"));
                     let registry = ::wasi_hyperium::poll::Poller::default();
                     let server = super::#server_type::new(super::#http_impl_struct_name);
                     let e = ::wasi_hyperium::hyperium1::handle_service_call(server, request, response_out, registry);
